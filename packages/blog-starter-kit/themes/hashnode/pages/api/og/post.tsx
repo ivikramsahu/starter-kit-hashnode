bf39040e51690b30401876545e35649ce9107875
Vikram Sahu ꩜ <snipperbytes@gmail.com>
2024-11-28 12:23:38 +0530

    Update post.tsx
    updated font

--- packages/blog-starter-kit/themes/hashnode/pages/api/og/post.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/api/og/post.tsx
@@ -8,23 +8,23 @@ export const config = {
 };
 
 const fontRegular = fetch(
-	new URL('../../../assets/Rebond\ Grotesque\ Medium.otf', import.meta.url),
+	new URL('../../../assets/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 const fontMedium = fetch(
-	new URL('../../../assets/PlusJakartaSans-Medium.ttf', import.meta.url),
+	new URL('../../../assets/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 const fontSemiBold = fetch(
-	new URL('../../../assets/PlusJakartaSans-SemiBold.ttf', import.meta.url),
+	new URL('../../../assets/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
-const fontBold = fetch(new URL('../../../assets/PlusJakartaSans-Bold.ttf', import.meta.url)).then(
+const fontBold = fetch(new URL('../../../assets/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf', import.meta.url)).then(
 	(res) => res.arrayBuffer(),
 );
 
 const fontExtraBold = fetch(
-	new URL('../../../assets/PlusJakartaSans-ExtraBold.ttf', import.meta.url),
+	new URL('../../../assets/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
@@ -67,7 +67,7 @@ export default async function handler(req: NextRequest) {
 		(
 			<div
 				style={{
-					fontFamily: '"Rebond Grotesque"',
+					fontFamily: '"ES Rebond Grotesque TRIAL"',
 					backgroundColor: bannerBackground,
 				}}
 				tw="relative flex h-full w-full flex-col p-8 subpixel-antialiased"
